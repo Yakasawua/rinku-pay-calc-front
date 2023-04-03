354285943dd2f71aff6a01b16e54233354f56a98
Jesus <yakasawua@hotmail.com>
2023-04-03 16:46:12 -0600

    Created component of the form to add employees

--- src/app/portal/portal-routing.module.ts
+++ src/app/portal/portal-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PortalComponent } from './portal.component';
 import { EmployeesComponent } from './containers/employees/employees.component';
+import { EmployeesFormComponent } from './containers/employees-form/employees-form.component';
 
 const routes: Routes = [
     {
@@ -10,6 +11,7 @@ const routes: Routes = [
         children: [
             //Employees
             {path: 'employees', component: EmployeesComponent},
+            {path: 'employees/:id', component: EmployeesFormComponent},
         ],
     },
 ];
